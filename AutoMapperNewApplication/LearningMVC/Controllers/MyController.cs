8ef800eb69dedb8725644dc0f1968c82ebc64d45
Akhil Mittal <akhil.mittal20@gmail.com>
2015-05-11 15:30:18 +0530

    Source Code and Database scripts
    Source Code and Database scripts added.

---
+++ AutoMapperNewApplication/LearningMVC/Controllers/MyController.cs
@@ -0,0 +1,182 @@
+﻿#region Using Namespaces
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web.Mvc;
+using AutoMapper;
+
+#endregion
+
+namespace LearningMVC.Controllers
+{
+    /// <summary>
+    /// My Controller to perform CRUD operations in MVC with the help of LINQ to SQL.
+    /// </summary>
+    public class MyController : Controller
+    {
+        #region Display...
+        /// <summary>
+        /// Get Action for index
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            Mapper.CreateMap<LearningMVC.User, LearningMVC.Models.User>();
+            var dbContext = new MyDBDataContext();
+            var userList = from user in dbContext.Users select user;
+            var users = new List<LearningMVC.Models.User>();
+            if (userList.Any())
+            {
+                foreach (var user in userList)
+                {
+                    LearningMVC.Models.User userModel = Mapper.Map<LearningMVC.User, LearningMVC.Models.User>(user);
+                    users.Add(userModel);
+                }
+            }
+          
+            return View(users);
+        }
+
+        /// <summary>
+        /// Get Action for Details
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Details(int? id)
+        {
+            var dbContext = new MyDBDataContext();
+            Mapper.CreateMap<LearningMVC.User, LearningMVC.Models.User>();
+            var userDetails = dbContext.Users.FirstOrDefault(userId => userId.UserId == id);
+            LearningMVC.Models.User user = Mapper.Map<LearningMVC.User, LearningMVC.Models.User>(userDetails);
+            return View(user);
+        } 
+        #endregion
+
+        #region Create...
+        /// <summary>
+        /// Get Action for Create
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Post Action for Create
+        /// </summary>
+        /// <param name="userDetails"> </param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Create(LearningMVC.Models.User userDetails)
+        {
+            try
+            {
+                Mapper.CreateMap<LearningMVC.Models.User, LearningMVC.User>();
+                var dbContext = new MyDBDataContext();
+                var user = Mapper.Map<LearningMVC.Models.User, LearningMVC.User>(userDetails);
+                dbContext.Users.InsertOnSubmit(user);
+                dbContext.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        } 
+        #endregion
+
+        #region Edit...
+        /// <summary>
+        /// Get Action for Edit
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Edit(int? id)
+        {
+            Mapper.CreateMap<LearningMVC.User, LearningMVC.Models.User>();
+            var dbContext = new MyDBDataContext();
+            var userDetails = dbContext.Users.FirstOrDefault(userId => userId.UserId == id);
+            var user = Mapper.Map<LearningMVC.User, LearningMVC.Models.User>(userDetails);
+            return View(user);
+        }
+
+        /// <summary>
+        /// Post Action for Edit
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Edit(int? id, LearningMVC.Models.User userDetails)
+        {
+            TempData["TempData Name"] = "Akhil";
+
+            try
+            {
+                var dbContext = new MyDBDataContext();
+                var user = dbContext.Users.FirstOrDefault(userId => userId.UserId == id);
+                if (user != null)
+                {
+                    user.FirstName = userDetails.FirstName;
+                    user.LastName = userDetails.LastName;
+                    user.Address = userDetails.Address;
+                    user.PhoneNo = userDetails.PhoneNo;
+                    user.EMail = userDetails.EMail;
+                    user.Company = userDetails.Company;
+                    user.Designation = userDetails.Designation;
+                    dbContext.SubmitChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        } 
+        #endregion
+
+        #region Delete...
+        /// <summary>
+        /// Get Action for Delete
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Delete(int? id)
+        {
+            var dbContext = new MyDBDataContext();
+            Mapper.CreateMap<LearningMVC.User, LearningMVC.Models.User>();
+            var userDetails = dbContext.Users.FirstOrDefault(userId => userId.UserId == id);
+            var user = Mapper.Map<LearningMVC.User, LearningMVC.Models.User>(userDetails);
+            return View(user);
+        }
+
+        /// <summary>
+        /// Post Action for Delete
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="userDetails"> </param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Delete(int? id, LearningMVC.Models.User userDetails)
+        {
+            try
+            {
+                var dbContext = new MyDBDataContext();
+                var user = dbContext.Users.FirstOrDefault(userId => userId.UserId == id);
+                if (user != null)
+                {
+                    dbContext.Users.DeleteOnSubmit(user);
+                    dbContext.SubmitChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        } 
+        #endregion
+    }
+}
---
+++ AutoMapperNewApplication/LearningMVC/Models/User.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace LearningMVC.Models
+{
+    #region User Model...
+    /// <summary>
+    /// User Model Class, purposely used for populating views and carry data from database.
+    /// </summary>
+    public class User
+    {
+        #region Automated Properties
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "FirstName is required")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EMail { get; set; }
+        public string Address { get; set; }
+        public string PhoneNo { get; set; }
+        public string Company { get; set; }
+        public string Designation { get; set; } 
+        #endregion
+    } 
+    #endregion
+}
